c633219f286a931c13bffaf110f1813f23d82cbf
hanifrahmath <45779009+hanifrahmath@users.noreply.github.com>
2019-01-06 11:42:13 +0700

    Add files via upload

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,31 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Brand Sepeda</a>
+<ul>
+<li><a href="create-brand.php">Create Brand</a></li>
+<li><a href="manage-brands.php">Menejemen Brand</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Sepeda</a>
+					<ul>
+						<li><a href="post-avehical.php">Upload Sepeda</a></li>
+						<li><a href="manage-vehicles.php">Menejemen Sepeda</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Menejemen Peminjaman</a></li>
+
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Menejemen Testimonial</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop""></i> Menejemen Pertanyaan</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Registrasi User</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Menejemen Halaman</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Info Kontak</a></li>
+
+			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Menejemen Subscribers</a></li>
+
+			</ul>
+		</nav>
